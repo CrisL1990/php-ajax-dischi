638e6e89e0197280b3d02c37e4daea0cc245a6d1
CrisL1990 <lucchetta.cris@gmail.com>
2022-03-14 19:46:30 +0100

    Milestone 1 completata

--- index_php_version.php
+++ index_php_version.php
@@ -8,24 +8,45 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="./style.css">
     <title>Dischi PHP</title>
 </head>
 <body>
-    <header>
-
+    
+    <header class="d-flex justify-content-end align-items-center px-5">
+        <select name="Select genre" id="select">
+                <option>Valore 1</option>
+        </select>
     </header>
 
-    <main>
+    <main class="container">
+       
+        <div class="row">
 
-        <?php 
-            foreach($database as $album){
-                echo "<p>" . $album['title'] . "</p>";
-                echo "<p>" . $album['author'] . "</p>";
-                echo "<p>" . $album['year'] . "</p>";
-                echo "<p>" . $album['genre'] . "</p>";
-                echo "<img src='" . $album['poster'] . "'>";
-            }
-        ?>
+            <?php
+
+                foreach($database as $album){
+
+                    echo "<div class='card'>";
+
+                        echo "<img class='card-img-top' src='" . $album['poster'] . "'>";
+
+                        echo "<div class='card-body'>";
+
+                            echo "<h5 class='card-title title'>" . $album['title'] . "</h5>";
+                            echo "<p class='card-text'>" . $album['author'] . "</p>";
+                            echo "<p class='card-text'>" . $album['year'] . "</p>";
+                            
+                        echo "</div>";
+
+                    echo "</div>";
+
+                }
+            ?>
+            
+
+        </div>
 
     </main>
 </body>
